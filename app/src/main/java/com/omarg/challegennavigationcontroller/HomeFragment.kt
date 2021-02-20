302b810a53071057ed9bb55b8ce24188c401415c
ZeusSura <Alejandrogzh@gmail.com>
2021-02-19 18:03:55 -0600

    Se crearon los fragments

--- app/src/main/java/com/omarg/challegennavigationcontroller/HomeFragment.kt
+++ app/src/main/java/com/omarg/challegennavigationcontroller/HomeFragment.kt
@@ -5,18 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.omarg.challegennavigationcontroller.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding.iniciarBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_nameFragment)
+        }
+        binding.terminosBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_terminosFragment)
+        }
+        return binding.root
     }
 
 }
